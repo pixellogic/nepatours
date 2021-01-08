0193b64af7213aabfbf6dbbc558bc0367aecb462
Ashish Bajracharya <lsr.files1@gmail.com>
2021-01-08 11:59:44 +0545

    header added

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className = "header">
+            <div className = "header__logo">
+                NepaTours
+            </div>
+            <div className = "heading__primary">
+                <span className = "heading__primary--main">outdoors</span>
+                <span className = "heading__primary--sub">is where life happens</span>
+            </div>
+        </div>
+    )
+}
